2dbcec421e3685343621eeca7525de4d1fcf4e2a
tanish0007 <jangratanish0007@gmail.com>
2025-03-06 17:04:29 +0530

    set .env

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,9 @@ import {createServer} from 'node:http';
 import {Server} from 'socket.io';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const server = createServer(app);
@@ -15,6 +18,8 @@ app.get('/home', (req,res) => {
 })
 
 const start = async () => {
+    const connectionDB = await mongoose.connect(process.env.MONGO_URL);
+
     server.listen(app.get("PORT"), () => {
         console.log(`Listening on port: ${app.get("PORT")}`);
     })
